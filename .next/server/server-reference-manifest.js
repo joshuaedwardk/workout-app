51f3ec06a9f5254a7f4d7733746b85376e7ed82c
Josh <joshuaedwardk@gmail.com>
2025-08-08 12:03:18 -0700

    Added .gitignore and fixed file paths

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"TEngEC2J5BvAOdFIAXECEnrlM0CN1C0iPOiby7moa4w=\"\n}"
--- .next/static/chunks/src_app_dashboard_page_tsx_45a68a45._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_b1ac0bb2.js",
-  "static/chunks/node_modules_418d5a07._.js",
-  "static/chunks/src_9fb88fe2._.js"
-],
-    source: "dynamic"
-});
