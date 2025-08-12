571b09b63ef36b0c06aa2eca331e30bf4d9a8de5
Josh <joshuaedwardk@gmail.com>
2025-08-11 17:03:48 -0700

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Joshua Workout Application",
-  description: "Interesting",
+  title: "Workout Application",
+  description: "Joshua E Workout Application, moving away from Airtable",
 };
 
 export default function RootLayout({
